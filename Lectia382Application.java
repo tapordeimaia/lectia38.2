d7c5ebd2059605174e3e79dffd5e84dd3f871075
tapordeimaia <146013799+tapordeimaia@users.noreply.github.com>
2024-01-02 12:28:05 +0200

    Add files via upload

---
+++ Lectia382Application.java
@@ -0,0 +1,14 @@
+package com.example.lectia38_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lectia382Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Lectia382Application.class, args);
+    }
+
+}
---
+++ Person.java
@@ -0,0 +1,24 @@
+package com.example.lectia38_2;
+public class Person {
+
+    String name;
+    String Surname;
+
+    int varsta;
+
+
+    public Person(String name, String surname, int varsta) {
+        this.name = name;
+        Surname = surname;
+        this.varsta = varsta;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", Surname='" + Surname + '\'' +
+                ", varsta=" + varsta +
+                '}';
+    }
+}
